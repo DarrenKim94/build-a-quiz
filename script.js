51f913e4e333787115108fa08deb8ed01c35487b
Darren Kim <dkim555@yahoo.com>
2023-02-02 19:55:12 -0800

    code question three

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-let q1a = document.getElementById('q1Answer').innerHTML;
-let q1b = document.getElementById('q2Answer').innerHTML;
-let q1c = document.getElementById('q3Answer').innerHTML;
-
-
-
 function submitAnswers() {
     if (document.getElementById('q1Answer').checked) {
         document.getElementById('q1Text').innerHTML = 'Correct!'
     } else {
-        document.getElementById('q1Text').innerHTML = 'Incorrect. The correct answer was '
+        document.getElementById('q1Text').innerHTML = 'Incorrect. The correct answer was Blue. '
+    }
+
+    if (document.getElementById('q2Answer').checked) {
+        document.getElementById('q2Text').innerHTML = 'Correct!'
+    } else {
+        document.getElementById('q2Text').innerHTML = 'Incorrect. The correct answer was 6. '
+    }
+    
+    if (document.getElementById('q3Answer').checked) {
+        document.getElementById('q3Text').innerHTML = 'Correct!'
+    } else {
+        document.getElementById('q3Text').innerHTML = 'Incorrect. The correct answer was 2. '
     }
     return false
 }
