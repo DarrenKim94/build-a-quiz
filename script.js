d5af7c381023b8c9f890c27da792f43111421155
Darren Kim <dkim555@yahoo.com>
2023-02-02 19:42:51 -0800

    define variables

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+let q1a = document.getElementById('q1Answer').innerHTML;
+let q1b = document.getElementById('q2Answer').innerHTML;
+let q1c = document.getElementById('q3Answer').innerHTML;
+
+
+
 function submitAnswers() {
     if (document.getElementById('q1Answer').checked) {
         document.getElementById('q1Text').innerHTML = 'Correct!'
-    } 
+    } else {
+        document.getElementById('q1Text').innerHTML = 'Incorrect. The correct answer was '
+    }
     return false
 }
